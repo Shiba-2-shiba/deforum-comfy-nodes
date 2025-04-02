587b3b835051f18d6965d38fa09f294e89fce783
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-04-02 09:22:21 +0900

    Update deforum_node_base.py

--- deforum_nodes/modules/deforum_node_base.py
+++ deforum_nodes/modules/deforum_node_base.py
@@ -9,8 +9,10 @@ class DeforumDataBase:
     OUTPUT_NODE = True
     CATEGORY = f"deforum/parameters"
     @classmethod
-    def IS_CHANGED(cls, text, autorefresh, **kwargs):
+    # def IS_CHANGED(cls, text, autorefresh, **kwargs):
+    def IS_CHANGED(cls, **kwargs): # 再改訂後のシグネチャ
         # Force re-evaluation of the node
+        autorefresh = kwargs.get('autorefresh', 'No')
         if autorefresh == "Yes":
             return float("NaN")
         return None
